8b7f6670e5c44353cdb4ff3fed56c4447607370d
Dustin Spicuzza <dustin@virtualroadside.com>
2015-09-02 20:29:44 -0400

    Add single batch file

---
+++ run_single.bat
@@ -0,0 +1,16 @@
+@echo off
+
+setlocal
+
+if %1 == "" echo "Usage: run_single.bat TEST_NUMBER"
+if %1 exit 1
+
+cd %~dp0\tests
+set PYTHONPATH=..\src
+set TESTNAME=test_challenges.py::test_%1
+
+for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
+
+py -3 -m pytest -l %ALL_BUT_FIRST% %1
+
+endlocal
