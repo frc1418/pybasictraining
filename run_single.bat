a94bb09ecf67ac4f40163255d7101f483426b72f
Dustin Spicuzza <dustin@virtualroadside.com>
2015-09-02 20:35:18 -0400

    All the fixes

--- run_single.bat
+++ run_single.bat
@@ -2,8 +2,8 @@
 
 setlocal
 
-if %1 == "" echo "Usage: run_single.bat TEST_NUMBER"
-if %1 exit 1
+if "%1" == "" echo Usage: run_single.bat TEST_NUMBER
+if "%1" == "" goto end
 
 cd %~dp0\tests
 set PYTHONPATH=..\src
@@ -11,6 +11,8 @@ set TESTNAME=test_challenges.py::test_%1
 
 for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
 
-py -3 -m pytest -l %ALL_BUT_FIRST% %1
+py -3 -m pytest -l %ALL_BUT_FIRST% %TESTNAME%
+
+:end
 
 endlocal
